c5fd7a7cd1b2856ccb6310488831453bb333cfa8
tyler thibault <tyler.thibault@protonmail.com>
2022-05-25 11:53:21 -0700

    added student login from instructor

--- flask_app/controllers/controller_student.py
+++ flask_app/controllers/controller_student.py
@@ -1,17 +1,17 @@
 from flask_app import app, bcrypt
-from flask_app.config.utils import login_admin_required
+from flask_app.config.utils import login_required
 from flask import render_template, redirect, session, request
 
 from flask_app.models import model_student, model_user
 
 @app.route('/student/new')          
-@login_admin_required
+@login_required
 def student_new():
     context = {}
     return render_template('student_new.html', **context)
 
 @app.route('/student/create', methods=['POST'])          
-@login_admin_required
+@login_required
 def student_create():
     cohort_id = request.form['cohort_id']
     if not model_student.Student.validate(request.form):
@@ -38,29 +38,32 @@ def bulk_add():
     return render_template('admin/student_bulk.html')
 
 @app.route('/student/<int:id>')          
-@login_admin_required
-def student_show(id):
-    context = {}
-    return render_template('student_show.html', **context)
+@login_required
+def student_login(id):
+    student = model_student.Student.get_one(id=id)
+    
+    session['uuid'] = student.user.id
+    session['level'] = student.user.level
+    return redirect('/')
 
 @app.route('/student/<int:id>/edit')          
-@login_admin_required
+@login_required
 def student_edit(id):
     context = {}
     return render_template('student_edit.html', **context)
 
 @app.route('/student/<int:id>/update', methods=['POST'])          
-@login_admin_required
+@login_required
 def student_update(id):
     return redirect('/')
 
 @app.route('/student/<int:id>/delete')          
-@login_admin_required
+@login_required
 def student_delete(id):
     return redirect('/')
 
 @app.route('/student/<int:id>/<int:cohort_id>/remove')          
-@login_admin_required
+@login_required
 def student_remove(id, cohort_id):
     model_student.Student.update_one(cohort_id=None, id=id)
     return redirect(f'/cohort/{cohort_id}/edit')
