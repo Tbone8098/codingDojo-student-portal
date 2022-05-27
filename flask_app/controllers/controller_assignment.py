7d8dd8758078bbf92ff5bad843a3e0896a111965
tyler thibault <tyler.thibault@protonmail.com>
2022-05-27 14:56:32 -0700

    added assignments

---
+++ flask_app/controllers/controller_assignment.py
@@ -0,0 +1,63 @@
+from flask_app import app
+from flask_app.config.utils import login_required
+from flask import render_template, redirect, session, request, jsonify
+
+from flask_app.models import model_assignment
+
+@app.route('/assignment/new')          
+@login_required
+def assignment_new():
+    return render_template('assignment_new.html')
+
+@app.route('/assignment/create', methods=['POST'])          
+@login_required
+def assignment_create():
+    return redirect('/')
+
+@app.route('/assignment/<int:id>')          
+@login_required
+def assignment_show(id):
+    return render_template('assignment_show.html')
+
+@app.route('/assignment/<int:id>/edit')          
+@login_required
+def assignment_edit(id):
+    return render_template('assignment_edit.html')
+
+@app.route('/assignment/<int:id>/update', methods=['POST'])          
+@login_required
+def assignment_update(id):
+    return redirect('/')
+
+@app.route('/api/assignment/<int:id>/update', methods=['POST'])          
+@login_required
+def api_assignment_update(id):
+    all_errors = {}
+
+    input_dict = {
+        'name': model_assignment.Assignment.validate_name_api, 
+        }
+
+    for item in input_dict:
+        if item in request.form:
+            errors = input_dict[item](request.form)
+            if len(errors) > 0:
+                for key in errors:
+                    all_errors[key] = errors[key]
+
+    if len(all_errors):
+        res = {
+            'status': 404,
+            'errors': all_errors
+        }
+        return jsonify(res)
+    model_assignment.Assignment.update_one(**request.form, id=id)
+    res = {
+        'status': 200
+    }
+    return jsonify(res)
+
+@app.route('/assignment/<int:id>/delete')          
+@login_required
+def assignment_delete(id):
+    return redirect('/')
